c81afe70932a2008c38776380062e5d820b7b14f
Eray Gökgör <gokgoreray@gmail.com>
2024-05-03 20:56:40 +0300

    add multiple run functionality returning mean

--- utils.py
+++ utils.py
@@ -1,15 +1,21 @@
 import time
+import numpy as np
 
 
-def function_timer(func: callable) -> callable:
+def function_timer(func):
     """
-    Decorator to measure the time a function takes to execute.
-    :param func: Function to measure the time it takes to execute.
-    :return: Wrapper function that measures the time the function takes to execute.
+    Decorator to run a function multiple times and get the mean result of the executions.
+    :param func: Function to decorate.
+    :return: Decorator function.
     """
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        print(f'Executed in {round(time.time() - start, 2)} seconds.')
+    def wrapper(*args, n=1, **kwargs):
+        results = []
+        for _ in range(n):
+            start = time.time()
+            result = func(*args, **kwargs)
+            results.append(time.time() - start)
+        mean_time = np.mean(results)
+        std_dev = np.std(results)
+        print(f'Mean time for {n} runs: {round(mean_time, 2)} ± {round(std_dev, 2)} seconds.')
         return result
     return wrapper
